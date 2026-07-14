5a160bdc46e276d9a962119a846465b31f38d892
gennadijvaliev02-design <gennadijvaliev02@gmail.com>
2026-07-14 06:17:24 +0300

    Delete duplicate Joker cancel compatibility layer

--- joker-panel-cancel.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  "use strict";
-
-  /*
-   * Compatibility placeholder.
-   * Android uses the visible cancel button and Escape handler from android-joker-v10.js.
-   * The former invisible top-right pointer zone was removed to prevent duplicate cancellation.
-   */
-})();
